37a0fb186fc2a9f6b2f228bda0a7444cbc0fa49b
Alejandro Baez <nativo.alejandro@gmail.com>
2021-03-13 09:38:28 -0500

    added html layout

---
+++ frontend/src/App.js
@@ -0,0 +1,95 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="circle-panel">
+        <ul className="circle">
+          <li>
+            <div className="text">1</div>
+          </li>
+          <li>
+            <div className="text">2</div>
+          </li>
+          <li>
+            <div className="text">3</div>
+          </li>
+          <li>
+            <div className="text">4</div>
+          </li>
+          <li>
+            <div className="text">5</div>
+          </li>
+          <li>
+            <div className="text">6</div>
+          </li>
+          <li>
+            <div className="text">7</div>
+          </li>
+          <li>
+            <div className="text">8</div>
+          </li>
+          <li>
+            <div className="text">9</div>
+          </li>
+          <li>
+            <div className="text">10</div>
+          </li>
+          <li>
+            <div className="text">11</div>
+          </li>
+          <li>
+            <div className="text">12</div>
+          </li>
+        </ul>
+        <ul className="inner-circle">
+          <li>
+            <div className="text">1</div>
+          </li>
+          <li>
+            <div className="text">2</div>
+          </li>
+          <li>
+            <div className="text">3</div>
+          </li>
+          <li>
+            <div className="text">4</div>
+          </li>
+          <li>
+            <div className="text">5</div>
+          </li>
+          <li>
+            <div className="text">6</div>
+          </li>
+          <li>
+            <div className="text">7</div>
+          </li>
+          <li>
+            <div className="text">8</div>
+          </li>
+          <li>
+            <div className="text">9</div>
+          </li>
+          <li>
+            <div className="text">10</div>
+          </li>
+          <li>
+            <div className="text">11</div>
+          </li>
+          <li>
+            <div className="text">12</div>
+          </li>
+        </ul> 
+      </div>
+      <div className="scale-panel">
+        <div>1 - 2 - 3 - 4 - 5 - 6 - 7 - 8</div>
+        <div>
+          <button>play</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
