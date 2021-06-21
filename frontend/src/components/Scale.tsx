a492eed429d850b73a520d969d90f5038233071b
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-21 15:37:06 -0500

    removed pkg lock json

--- frontend/src/components/Scale.tsx
+++ frontend/src/components/Scale.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Tone from './Tone'
 type ScaleProps = {
 	children: React.ReactNode,
 	name: String
 }
 export default function Scale(props: ScaleProps) {
+	
 	console.log('lolo',props.children)
 	return <div style={{ display: 'flex', gap: 30 }}>
 		{props.children && <p className="scale-text">{props.name}</p>}
-		<p className="scale-text">{props.children}</p>
+		{/* <p className="scale-text">{props.children}</p> */}
+		{React.Children.map(props.children, (note: any) => {
+			return <Tone note={note} />
+		})}
 	</div>
 }
--- frontend/src/components/Tone.tsx
+++ frontend/src/components/Tone.tsx
@@ -1,10 +1,13 @@
 type ToneProps = {
-	note: String,
-	onClick: (note: String) => void
+	note: String;
+	// onClick: (note: String) => void
 }
 export default function Tone(props: ToneProps) {
+	function setTriad() {
+		return console.log('tone', props.note)
+	}
 	return (
-		<li onClick={() => props.onClick(props.note)}>
+		<li onClick={setTriad}>
 			<div className="text">{props.note}</div>
 		</li>
 	)
